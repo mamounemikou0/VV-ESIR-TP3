7636f55042ffc9d9ece486eb11a1034fea043abe
mamounemikou0 <130618888+mamounemikou0@users.noreply.github.com>
2024-10-11 18:54:03 +0200

    Update StringUtilsTest.java

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -1,11 +1,42 @@
 package fr.istic.vv;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
-import static fr.istic.vv.StringUtils.isBalanced;
-import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+	 @Test
+	    public void testEmptyString() {
+	        assertTrue(StringUtils.isBalanced(""));
+	    }
 
+	    @Test
+	    public void testSingleInvalidCharacters() {
+	        assertFalse(StringUtils.isBalanced("("));
+	        assertFalse(StringUtils.isBalanced("]"));
+	        assertFalse(StringUtils.isBalanced("{"));
+	    }
+
+	    @Test
+	    public void testCorrectlyNestedStringUtils() {
+	        assertTrue(StringUtils.isBalanced("{}"));
+	        assertTrue(StringUtils.isBalanced("{[]}"));
+	        assertTrue(StringUtils.isBalanced("{[()]}"));
+	    }
+
+	    @Test
+	    public void testIncorrectlyNestedOrUnmatched() {
+	        assertFalse(StringUtils.isBalanced("]{"));
+	        assertFalse(StringUtils.isBalanced("[(])"));
+	        assertFalse(StringUtils.isBalanced("{(}]"));
+	    }
+
+	    @Test
+	    public void testRandomStrings() {
+	        assertTrue(StringUtils.isBalanced("a(b)c"));
+	        assertTrue(StringUtils.isBalanced("foo{bar}"));
+	        assertFalse(StringUtils.isBalanced("{foo[bar]}]"));
+	    }
 }
